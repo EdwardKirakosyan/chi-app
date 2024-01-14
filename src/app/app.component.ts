dc4a780f349a13b465419ccaf2c3491c040d0ba4
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-14 17:44:48 +0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChildren, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chi-app';
+  fullName: string = '';
+  @ViewChildren('inputEl', {}) inputElements?: QueryList<ElementRef>;
+
+  show() {
+    let name = ''
+    this.inputElements?.forEach((el) => {
+      name += el.nativeElement.value + ' '
+    })
+    this.fullName = name.trim();
+  }
 }
